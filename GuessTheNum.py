12928d190290aec65dd30ba743477ddf90628331
DESKTOP-VM4D9GJ <jolyndonjr@gmail.com>
2021-12-08 02:10:58 +0800

    Added Text Color and Adjusted Print Statements

--- GuessTheNum.py
+++ GuessTheNum.py
@@ -7,19 +7,27 @@ import random as rnd
 # Display “Less than” if the inputed number is less than the random number
 # Repeat asking the user until the random number has been guessed correctly.
 
-mysteryNum = rnd.randint(0,100)
+Red = "\33[91m" # Decorative Variable Group
+Grn = "\33[92m"
+Yllw = "\33[93m"
+Blue = "\33[94m"
+End = "\33[0m"
+Itlc = "\33[3m"
+Bldtxt = "\33[1m"
 
-while mysteryNum != "guessed":
-    Usr_Guess = input("Guess the number: ")
+mysteryNum = rnd.randint(0,100) # Generates a Random Number; serves as 'counter' variable to Loop
+                    
+while mysteryNum != "guessed": # Continue iteration if the value is unchanged
+    Usr_Guess = input("\nGuess the number: ")
     if Usr_Guess.isdecimal() == True:
         if int(Usr_Guess) != mysteryNum:
-            if int(Usr_Guess) < mysteryNum:
-                print("Less than")
-            elif int(Usr_Guess) > mysteryNum:
-                print("Greater than")
+            if int(Usr_Guess) < mysteryNum: # Display Criteria 1
+                print(f"{Red}Less than{End}")
+            elif int(Usr_Guess) > mysteryNum: # Display Criteria 2
+                print(f"{Grn}Greater than{End}")
         elif int(Usr_Guess) == mysteryNum:
             mysteryNum = "guessed"
     else:
-        print("Invalid number")
+        print(f"{Red}Invalid number{End}")
 else:
-    print("Congratulations! You win!")
+    print(f"{Bldtxt}{Yllw}Congratulations! You win!{End}m,")
